dd1c2113f48b418be700ce16daf88a3106d752d1
Alexander Galichev <lex.galichev@gmail.com>
2022-06-30 11:58:30 +1000

    Unit 8.6 - Task 8.6.1: Folder cleaning + FolderTask1 for example.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -12,45 +12,45 @@
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine($"{path}\n");
                 Console.ResetColor();
-                Console.WriteLine("Выберете действие:\n <F1> Указать директорию\n <F2> Удалить папки и файлы, неиспользуемые более 30 минут, в указанной директории\n <F3> Показать все файлы и подпапки и их время доступа в указанной директории\n <F4> Выйти из приложения ");
+                Console.WriteLine("Выберете действие:\n <1> Указать директорию\n <2> Удалить папки и файлы, неиспользуемые более 30 минут, в указанной директории\n <3> Показать все файлы и подпапки и их время доступа в указанной директории\n <4> Выйти из приложения ");
                 
                 var key = Console.ReadKey(true);
 
                 switch (key.Key)
                 {
-                    case ConsoleKey.F1: // Задание директории
+                    case ConsoleKey.D1: // Задание директории
 
                         path = GetPath();
                         continue;
 
-                    case ConsoleKey.F2: // Удаление папок и файлов
+                    case ConsoleKey.D2: // Удаление папок и файлов
 
                         Console.WriteLine();
                         Console.WriteLine("Поиск неиспользуемых папок и файлов");
                         Console.WriteLine();
 
-                        try
+                        DirectoryInfo diA = new DirectoryInfo(path);
+                        if (diA.Exists)
                         {
-                            DirectoryInfo diA = new DirectoryInfo(path);
-                            CleanDirectory(diA);
+                            if (diA.GetDirectories().Length != 0)
+                            {
+                                CleanDirectory(diA);
+                            }
+                            else if (diA.GetDirectories().Length == 0 && diA.GetFiles().Length == 0)
+                            {
+                                Console.WriteLine("Папка пуста");
+                            }
                         }
-                        catch(DirectoryNotFoundException ex)
+                        else
                         {
-                            Console.WriteLine($"Директория не найдена. Ошибка: {ex.Message}");
+                            Console.WriteLine($"Директории {diA.FullName} не существует");
                         }
-                        catch(UnauthorizedAccessException ex)
-                        {
-                            Console.WriteLine($"Нет доступа. Ошибка: {ex.Message}");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Ошибка: {ex.Message}");
-                        }
+                        Console.WriteLine();
                         Console.WriteLine("Нажмите любую клавишу для продолжения");
                         Console.ReadKey(true);
                         continue;
 
-                    case ConsoleKey.F3: // Время доступа всех файлов и папок в указанной директории, в том числе и подпапок
+                    case ConsoleKey.D3: // Время доступа всех файлов и папок в указанной директории, в том числе и подпапок
 
                         Console.WriteLine();
                         Console.WriteLine("Подкаталоги указанной директории:");
@@ -58,11 +58,12 @@
 
                         DirectoryInfo di = new DirectoryInfo(path);
                         ShowLastAccessTime(di);
+                        Console.WriteLine();
                         Console.WriteLine("Нажмите любую клавишу для продолжения");
                         Console.ReadKey(true);
                         continue;
 
-                    case ConsoleKey.F4: // Выход
+                    case ConsoleKey.D4: // Выход
 
                         break;
 
@@ -80,36 +81,25 @@
             string path = "<..>";
 
             Console.Write("Укажите директорию: ");
-            //try
-            //{
-            path = Path.GetFullPath(Console.ReadLine()); 
-
-            if (path == "" || !new DirectoryInfo(path).Exists)
+            try
             {
-                Console.WriteLine("Несуществующий путь к каталогу");
-                path = "<..>";
+                path = Path.GetFullPath(Console.ReadLine());
+            
+                if (path == "" || !new DirectoryInfo(path).Exists)
+                {
+                   Console.WriteLine($"Директории {path} не существует");
+                   path = "<..>";
                     //continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Директория {new DirectoryInfo(path).FullName} найдена!");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                //DirectoryInfo dirInfo = new DirectoryInfo(path);
-                Console.WriteLine($"Директория найдена: {new DirectoryInfo(path).FullName}");
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
-               
-            //}
-            //catch(DirectoryNotFoundException ex)
-            //{
-            //    Console.WriteLine($"Директория не найдена. Ошибка: {ex.Message}");
-            //}
-            //catch(UnauthorizedAccessException ex)
-            //{
-            //    Console.WriteLine($"Отстствует доступ. Ошибка: {ex.Message}");
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.WriteLine($"Произошла ошибка: {ex.Message}");
-            //}
-
             Console.WriteLine("Нажмите любую клавишу для продолжения");
             Console.ReadKey(true);
             return path;
@@ -117,78 +107,108 @@
 
         static void CleanDirectory(DirectoryInfo directory)
         {
-            DirectoryInfo[] di = directory.GetDirectories();
-            FileInfo[] fi = directory.GetFiles();
-            
-            // За счёт рекурсии сначала происходит обнаружение и проверка всех файлов в указанной папке и всех подпапках
-
-            foreach (FileInfo f in fi)
+            try
             {
-                try
+                DirectoryInfo[] di = directory.GetDirectories();
+                FileInfo[] fi = directory.GetFiles();
+
+                foreach (var subfolder in di)
                 {
-                    if (DateTime.Now.Subtract(File.GetLastAccessTime(f.FullName)) > TimeSpan.FromMinutes(30))
+                    try
                     {
-                        Console.WriteLine($"Файл {f.Name} удалён!");
-                        f.Delete();
+                        if (DateTime.Now.Subtract(subfolder.LastAccessTime) > TimeSpan.FromMinutes(30))
+                        {
+                            Console.WriteLine($"Удаление папки {subfolder.Name}!\n");
+                            subfolder.Delete(true);
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Папка {subfolder.Name} ещё используется!\n");
+                            CleanDirectory(subfolder);
+                        }
                     }
-                    else
+                    catch (DirectoryNotFoundException ex)
                     {
-                        Console.WriteLine($"Файл {f.Name} используется!");
+                        Console.WriteLine($"Директория не найдена. Ошибка: {ex.Message}");
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Console.WriteLine($"Нет доступа. Ошибка: {ex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка: {ex.Message}");
                     }
                 }
-                catch (FileNotFoundException ex)
+
+                // Поиск файлов
+                    foreach (FileInfo f in fi)
                 {
-                    Console.WriteLine($"Файл не найден. Ошибка: {ex.Message}");
-                }
-                catch (UnauthorizedAccessException ex)
-                {
-                    Console.WriteLine($"Нет доступа. Ошибка: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Ошибка: {ex.Message}");
-                }
+                    try
+                    {
+                        if (DateTime.Now.Subtract(f.LastAccessTime) > TimeSpan.FromMinutes(30))
+                        {
+                            Console.WriteLine($"Удаление файла {f.Name}!\n");
+                            f.Delete();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Файл {f.Name} ещё используется!\n");
+                        }
+                    }
+                    catch (FileNotFoundException ex)
+                    {
+                        Console.WriteLine($"Файл не найден. Ошибка: {ex.Message}");
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Console.WriteLine($"Нет доступа. Ошибка: {ex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка: {ex.Message}");
+                    }
+                }   
             }
-
-            // После завершения рекурсии происходит проверка всех подпапок
-
-            foreach(var subfolder in di)
+            catch (UnauthorizedAccessException ex)
             {
-                CleanDirectory(subfolder);
-
-                try
-                {
-                    if (DateTime.Now.Subtract(Directory.GetLastAccessTime(subfolder.FullName)) > TimeSpan.FromMinutes(30) && subfolder.GetDirectories().Length == 0 && subfolder.GetFiles().Length == 0)
-                    {
-                        Console.WriteLine($"Каталог {subfolder.Name} удалён!");
-                        subfolder.Delete();
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Папка {subfolder.Name} ещё используется!");
-                    }
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine($"Ошибка: {ex.Message}");
-                }
+                Console.WriteLine($"Нет доступа. Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
         }
 
         static void ShowLastAccessTime(DirectoryInfo directory)
         {
-            var folders = directory.GetDirectories();
-            var files = directory.GetFiles();
-            foreach (var file in files)
+            try
             {
-                Console.WriteLine($"{file.Name}: {file.LastAccessTime}");
+                var folders = directory.GetDirectories();
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"{file.Name}: {file.LastAccessTime}");
+                }
+                Console.WriteLine();
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine($"{folder.Name}: {folder.LastAccessTime}");
+                    //DirectoryInfo[] subfolders = folder.GetDirectories();
+                    //Console.WriteLine(subfolders.Length);
+                    //FileInfo[] fileInfo = folder.GetFiles();
+                    //Console.WriteLine(fileInfo.Length);
+                    ShowLastAccessTime(folder);
+                }
             }
-
-            foreach (var folder in folders)
+            catch(DirectoryNotFoundException ex)
             {
-                //Console.WriteLine();
-                Console.WriteLine($"\n{folder.Name}: {folder.LastAccessTime}\n");
-                ShowLastAccessTime(folder);
+                Console.WriteLine($"Директория не существует. Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
         }
     }
